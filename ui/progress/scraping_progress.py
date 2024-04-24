10bd8146860ae93b2134f4aed648021b7e885ffb
Kyrrin <59748840+Kochisa@users.noreply.github.com>
2024-04-24 22:18:48 +0800

    Update scraping_progress.py

--- ui/progress/scraping_progress.py
+++ ui/progress/scraping_progress.py
@@ -10,12 +10,12 @@ if TYPE_CHECKING:
 
 
 async def adjust_title(s: str, length: int = 40, placeholder: str = "...") -> str:
-    """Collapse and truncate or pad the given string to fit in the given length"""
+    """折叠、截断或填充给定的字符串，以适应给定的长度"""
     return f"{s[:length - len(placeholder)]}{placeholder}" if len(s) >= length else s.ljust(length)
 
 
 class ScrapingProgress:
-    """Class that manages the download progress of individual files"""
+    """管理单个文件下载进度的类"""
     def __init__(self, visible_tasks_limit: int, manager: 'Manager'):
         self.manager = manager
 
@@ -26,23 +26,23 @@ class ScrapingProgress:
         self.progress_group = Group(self.progress, self.overflow, self.queue)
 
         self.color = "plum3"
-        self.type_str = "Files"
+        self.type_str = "链接"
         self.progress_str = "[{color}]{description}"
-        self.overflow_str = "[{color}]... And {number} Other Links"
-        self.queue_str = "[{color}]... And {number} Links In Scrape Queue"
-        self.overflow_task_id = self.overflow.add_task(self.overflow_str.format(color=self.color, number=0, type_str=self.type_str), visible=False)
-        self.queue_task_id = self.queue.add_task(self.queue_str.format(color=self.color, number=0, type_str=self.type_str), visible=False)
+        self.overflow_str = "[{color}]... 和其他链接数量: {number}"
+        self.queue_str = "[{color}]... 和抓取队列中的链接数量: {number}"
+        self.overflow_task_id = self.overflow.add_task(self.overflow_str.format(color=self.color, number=0), visible=False)
+        self.queue_task_id = self.queue.add_task(self.queue_str.format(color=self.color, number=0), visible=False)
 
         self.visible_tasks: List[TaskID] = []
         self.invisible_tasks: List[TaskID] = []
         self.tasks_visibility_limit = visible_tasks_limit
 
     async def get_progress(self) -> Panel:
-        """Returns the progress bar"""
-        return Panel(self.progress_group, title="Scraping", border_style="green", padding=(1, 1))
+        """返回进度条"""
+        return Panel(self.progress_group, title="抓取", border_style="green", padding=(1, 1))
 
     async def get_queue_length(self) -> int:
-        """Returns the number of tasks in the scraper queue"""
+        """返回抓取器队列中的任务数量"""
         total = 0
 
         for scraper in self.manager.scrape_mapper.existing_crawlers.values():
@@ -51,7 +51,7 @@ class ScrapingProgress:
         return total
 
     async def redraw(self) -> None:
-        """Redraws the progress bar"""
+        """重新绘制进度条"""
         while len(self.visible_tasks) > self.tasks_visibility_limit:
             task_id = self.visible_tasks.pop(0)
             self.invisible_tasks.append(task_id)
@@ -62,18 +62,18 @@ class ScrapingProgress:
             self.progress.update(task_id, visible=True)
 
         if len(self.invisible_tasks) > 0:
-            self.overflow.update(self.overflow_task_id, description=self.overflow_str.format(color=self.color, number=len(self.invisible_tasks), type_str=self.type_str), visible=True)
+            self.overflow.update(self.overflow_task_id, description=self.overflow_str.format(color=self.color, number=len(self.invisible_tasks)), visible=True)
         else:
             self.overflow.update(self.overflow_task_id, visible=False)
 
         queue_length = await self.get_queue_length()
         if queue_length > 0:
-            self.queue.update(self.queue_task_id, description=self.queue_str.format(color=self.color, number=queue_length, type_str=self.type_str), visible=True)
+            self.queue.update(self.queue_task_id, description=self.queue_str.format(color=self.color, number=queue_length), visible=True)
         else:
             self.queue.update(self.queue_task_id, visible=False)
 
     async def add_task(self, url: URL) -> TaskID:
-        """Adds a new task to the progress bar"""
+        """向进度条添加新任务"""
         if len(self.visible_tasks) >= self.tasks_visibility_limit:
             task_id = self.progress.add_task(self.progress_str.format(color=self.color, description=str(url)), visible=False)
             self.invisible_tasks.append(task_id)
@@ -84,7 +84,7 @@ class ScrapingProgress:
         return task_id
 
     async def remove_task(self, task_id: TaskID) -> None:
-        """Removes a task from the progress bar"""
+        """从进度条中移除任务"""
         if task_id in self.visible_tasks:
             self.visible_tasks.remove(task_id)
             self.progress.update(task_id, visible=False)
@@ -93,5 +93,5 @@ class ScrapingProgress:
         elif task_id == self.overflow_task_id:
             self.overflow.update(task_id, visible=False)
         else:
-            raise ValueError("Task ID not found")
+            raise ValueError("找不到任务ID")
         await self.redraw()
